892de1b46dc0f90c4a9b4b4b424e6a3d59424caa
popeyeio <szl199083@126.com>
2019-01-08 11:20:47 +0800

    add Choose

--- tools.go
+++ tools.go
@@ -3,8 +3,30 @@ package handy
 import (
 	"reflect"
 	"runtime"
+
+	"github.com/valyala/fastrand"
 )
 
 func GetFuncName(f interface{}) string {
 	return runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name()
+}
+
+func Choose(weights []uint32) (index int, ok bool) {
+	if len(weights) == 0 {
+		return
+	}
+
+	total := uint32(0)
+	for _, weight := range weights {
+		total += weight
+	}
+
+	n := fastrand.Uint32n(total) + 1
+	for i, w := range weights {
+		n -= w
+		if n <= 0 {
+			return i, true
+		}
+	}
+	return
 }
